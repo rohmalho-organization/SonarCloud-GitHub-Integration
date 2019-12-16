604f5e6da5c1cc9bf2fd1823b82199ee4a6678d4
rohmalho <58813147+rohmalho@users.noreply.github.com>
2019-12-16 16:44:52 +0530

    Update DefaultPageController.java

--- src/sonar/demo/controller/DefaultPageController.java
+++ src/sonar/demo/controller/DefaultPageController.java
@@ -7,6 +7,8 @@ public class DefaultPageController {
 	public void getDefaultPage() {
 		DefaultPageService.getDefaultPage();
 		System.out.print("dsddssd");
+		String test = null;
+		System.out.print(test.length());
 		//goto:Statement1;
 	}
 
